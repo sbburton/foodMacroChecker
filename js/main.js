c6cfc883ba4e88b59f4109eeb0d77d17d151c846
Scott Burton <burtonsb@gmail.com>
2022-04-18 22:31:40 -0400

    working macros

--- js/main.js
+++ js/main.js
@@ -20,8 +20,23 @@ function getFetch() {
   fetch(url)
     .then(res => res.json())
     .then(data => {
-      // console.log('all API data', data) // <- all data
+      console.log('all API data.product', data.product) // <- all data
       const item = new ProductInfo(data.product)
+
+      // healthy grades and leves
+      console.log(data.product.nutriscore_grade)
+      console.log(data.product.nutrient_levels)
+
+      // calories per serving
+      console.log(data.product.nutriments.energy_serving)
+
+      // macros per serving
+      console.log(data.product.nutriments.carbohydrates_serving)
+      console.log(data.product.nutriments.carbohydrates_unit)
+      console.log(data.product.nutriments.fat_serving)
+      console.log(data.product.nutriments.fat_unit)
+      console.log(data.product.nutriments.proteins_serving)
+      console.log(data.product.nutriments.proteins_unit)
       
       // call additional stuff if ingredients found
       if (data.status === 1) {
@@ -31,6 +46,8 @@ function getFetch() {
         
         // self check
         // console.log('item.ingredients', item.ingredients)
+
+        item.showMacros()
 
         // show all ingredients of item
         item.showIngredients()
@@ -50,6 +67,17 @@ class ProductInfo {
     this.name = productData.product_name
     this.ingredients = productData.ingredients
     this.image = productData.image_url
+
+    this.carbs = productData.nutriments.carbohydrates_serving
+    this.carbsUnit = productData.nutriments.carbohydrates_unit
+
+    this.fats = productData.nutriments.fat_serving
+    this.fatsUnit = productData.nutriments.fat_unit
+
+    this.proteins = productData.nutriments.proteins_serving
+    this.proteinsUnit = productData.nutriments.proteins_unit
+
+    this.calories =productData.nutriments.energy_serving
   }
 
   showInfo() {
@@ -66,13 +94,13 @@ class ProductInfo {
     }
 
     if (this.ingredients !== null) {
-     for (let key in this.ingredients) {
+      for (let key in this.ingredients) {
         // build ingredient table
         let newRow = tableRef.insertRow(-1)
         let newICell = newRow.insertCell(0)
         let newVCell = newRow.insertCell(1)
 
-        // get ingredients and 
+        // get ingredients and vegetarian statuses (stati?)
         let newIText = document.createTextNode(this.ingredients[key].text) 
         let vegStatus = this.ingredients[key].vegetarian
         let newVText = document.createTextNode(vegStatus)
@@ -82,4 +110,39 @@ class ProductInfo {
       }
     }
   }
+
+  // DISPLAY MACROS FUNCTION
+  showMacros() {
+    let tableRef = document.querySelector('#macros-table')
+
+    // delete (potential) rows from previous searches
+    for (let i = 1; i < tableRef.rows.length;) {
+      tableRef.deleteRow(i)
+    }
+
+    // ADD FATS, CARBS, PROTEISN & UNITS PER SERVING
+    if (this.fats !== null) {
+      let newRow = tableRef.insertRow(1)
+      let newFCell = newRow.insertCell(0)
+      let newFText = document.createTextNode(this.fats + " " + this.fatsUnit)
+      newFCell.appendChild(newFText)
+      
+      // ADD CARBS G PER SERVING
+      if (this.carbs !== null) {
+        let newCCell = newRow.insertCell(1)
+        let newCText = document.createTextNode(this.carbs + " " + this.carbsUnit)
+        newCCell.appendChild(newCText)
+      }
+
+      // ADD PROTEINS G PER SERVING
+      if (this.proteins !== null) {
+        let newPCell = newRow.insertCell(2)
+        let newPText = document.createTextNode(this.proteins + " " + this.proteinsUnit)
+        newPCell.appendChild(newPText)
+      }
+
+    }
+  }
+
+
 }
